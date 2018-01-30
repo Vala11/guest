e260e4d23bbec1448a0a4b2d1e9482851999834a
vala <850668984@qq.com>
2018-01-30 19:33:08 +0800

    1

--- sign/admin.py
+++ sign/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from sign.models import Event,Guest
 # Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['name', 'status', 'start_time','id']
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ['realname', 'phone','email','sign','create_time','event']
+admin.site.register(Event,Event)
+admin.site.register(Guest,Guest)
+
